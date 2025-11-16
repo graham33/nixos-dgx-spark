173e2c0e3657a986cf0398dd4ba6cb03f96aa065
Graham Bennett <graham@grahambennett.org>
2025-11-16 10:27:11 +0000

    Split out overlay, fix pytorch broken

--- flake.nix
+++ flake.nix
@@ -11,19 +11,7 @@
   };
 
   outputs = { self, nixpkgs, nixos-generators, flake-utils }: let
-    cuda13Overlay = final: prev: {
-      cudaPackages = prev.cudaPackages_13;
-
-      magma = prev.magma.overrideAttrs (oldAttrs: {
-        patches = (oldAttrs.patches or []) ++ [
-          (final.fetchpatch {
-            name = "magma-fix-cuda-version-detection.patch";
-            url = "https://github.com/icl-utk-edu/magma/commit/235aefb7b064954fce09d035c69907ba8a87cbcd.patch";
-            hash = "sha256-i9InbxD5HtfonB/GyF9nQhFmok3jZ73RxGcIciGBGvU="; # Replace with actual hash
-          })
-        ];
-      });
-    };
+    cuda13Overlay = import ./overlays/cuda-13.nix;
   in {
     # Expose the DGX Spark module for other projects
     nixosModules.dgx-spark = import ./modules/dgx-spark.nix;
